7774146b3e47cfe7984cdc9c018032f29c085205
juyung-kim <101319174+juyung-kim@users.noreply.github.com>
2022-03-10 18:02:35 +0900

    Delete coding/02 directory

--- coding/02/deep/predict.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-num = 100
-plt.imshow(x_test[num])
-plt.show()
-result = model.predict(x_test[num:num+1])
-print('label:', y_test[num:num+1])
-print('predict:', np.argmax(result))
